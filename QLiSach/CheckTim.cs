6117a7ad5d1b5ec8e1925bcd18fac409712bcee9
vodaolananh <1554050005anh@ou.edu.vn>
2019-04-15 18:15:05 +0700

    Tạo class check tìm

---
+++ QLiSach/CheckTim.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace QLiSach
+{
+    public class CheckTim
+    {
+        public static string checkTim(string maKH)
+        {
+            if (maKH.Length < 4)
+            {
+                return "Tìm thành công";
+            }
+
+            return "0";
+
+        }
+    }
+}
