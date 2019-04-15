b7e6362b825f6dbc497db6cd9f8a8930fc7790c3
vodaolananh <1554050005anh@ou.edu.vn>
2019-04-15 18:00:50 +0700

    Tạo class CheckXoa

--- QLiSach/CheckXoa.cs
+++ QLiSach/CheckXoa.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace QLiSach
 {
     public class CheckXoa
     {
+        public static string checkXoa(string maKH)
+        {
+            if (maKH.Length < 4)
+            {
+                return "Xóa thành công";
+            }
+
+            return "0";
+
+        }
 
     }
 }
