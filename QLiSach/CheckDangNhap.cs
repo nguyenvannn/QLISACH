23f3f037195aa46024ea7269143c048c6bd89569
van nguyen <nguyenvan0525@gmail.com>
2019-04-15 17:28:08 +0700

    Unit test Dang nhap

---
+++ QLiSach/CheckDangNhap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLiSach
+{
+    public class CheckDangNhap
+    {
+        public static string ktdangnhap(string ten, string pass)
+        {
+            if ((ten == "admin") && (pass == "")) { return ("Vui lòng điền mật khẩu"); }
+            else
+            if (ten == "") { return (" Sai tên đăng nhập hoặc sai mật khẩu"); }
+           
+
+            else return "0";
+        }
+    }
+}
---
+++ UnitTest_QLSach/DangNhap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QLiSach;
+using System.Windows.Forms;
+
+namespace UnitTest_QLSach
+{
+    [TestClass]
+    public class DangNhap
+    {
+        [TestMethod]
+        public void Test_Nhap_Dung_Tai_Khoan()
+        {
+            string actual = CheckDangNhap.ktdangnhap("admin", "12345");
+         
+            string expected = "0";
+            Assert.AreEqual(expected, actual);
+
+        }
+        
+    }
+}
