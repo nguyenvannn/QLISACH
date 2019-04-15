a118a6c03fa1a7657b760ecd48f888ec66f1465c
vodaolananh <1554050005anh@ou.edu.vn>
2019-04-15 18:21:06 +0700

    Class Check Sửa

---
+++ QLiSach/CheckSua.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace QLiSach
+{
+    public class CheckSua
+    {
+        public static string checkSua(string MaKH, string HoTen, string DiaChi, string DienThoai, string Email)
+        {
+            if (MaKH.Length > 4)
+            {
+                return "Mã KH quá dài";
+            }
+            else if (HoTen.Length > 30)
+            { return "Họ Tên quá dài"; }
+            else if (DiaChi.Length > 30)
+            { return "Địa Chỉ quá dài"; }
+            else if (DienThoai.Length > 7)
+            { return "SDT quá dài"; }
+            else if (Email.Length > 12)
+            { return "Email quá dài"; }
+            return "0";
+        }
+    }
+}
